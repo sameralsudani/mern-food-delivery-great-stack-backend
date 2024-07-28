4121268001ec311bc471a6abff55ce32f509f2e8
sameralsudani <samerxxxalsudani@gmail.com>
2024-07-27 22:50:17 -0500

    Updates

---
+++ config/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  await mongoose
+    .connect(
+      "mongodb+srv://samerxxxalsudani:Monk1234@cluster0.eravxrh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/food-del"
+    )
+    .then(() => console.log("DB Connected"));
+};
+
+// add your mongoDB connection string above.
+// Do not use '@' symbol in your databse user's password else it will show an error.
